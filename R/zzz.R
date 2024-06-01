789393bee05a67e7ae6de0ae1d5cdd489bccb704
jakeelement <9gse@queensu.ca>
2024-06-01 12:34:47 -0300

    updates to user guide

--- R/zzz.R
+++ R/zzz.R
@@ -1,9 +1,9 @@
-# startp message
+# startup message
 
 .onAttach <- function(libname, pkgname) {
   packageStartupMessage("Welcome to LobTag2! ",
                         " To get started, your User Guide can be found in: ",
-                        system.file("data", "user_guide.pptx", package = "LobTag2"),
+                        system.file("data", "user_guide.pdf", package = "LobTag2"),
                         " And your data entry templates in: ",
                         system.file("extdata", package = "LobTag2"),
                         "       ",
